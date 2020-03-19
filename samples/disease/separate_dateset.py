1bb6ef8643a206fb56f93d67f734b4e0776ad0ec
Chienping Tsung <jianpingzong@outlook.com>
2020-03-20 00:33:50 +0800

    Update separate_dateset.py

--- samples/disease/separate_dateset.py
+++ samples/disease/separate_dateset.py
@@ -1,19 +1,18 @@
+import argparse
 import json
 import os
 import random
 import shutil
 
 if __name__ == '__main__':
-    import argparse
-
-    # Parse command line arguments
-    parser = argparse.ArgumentParser(description='separate dataset into train and val.')
-    parser.add_argument('--dataset', required=True, help='directory  of the dataset')
-    parser.add_argument('--ratio', required=False, default=0.2, help='division ratio of val')
+    # Parse command line arguments.
+    parser = argparse.ArgumentParser(description="Separate dataset into train and val.")
+    parser.add_argument('--dataset', required=True, help="Path of the dataset.")
+    parser.add_argument('--ratio', default=0.2, required=False, help="Division ratio of val.")
     args = parser.parse_args()
 
     if not os.path.exists(args.dataset):
-        print("no directory exists.")
+        print("The dataset path does not exist.")
         exit(1)
 
     if not os.path.exists(os.path.join(args.dataset, 'train')):
@@ -21,27 +20,27 @@ if __name__ == '__main__':
     if not os.path.exists(os.path.join(args.dataset, 'val')):
         os.mkdir(os.path.join(args.dataset, 'val'))
 
-    json_file_names = [i for i in os.listdir(args.dataset) if i.endswith('.json')]
+    json_file_names = [name for name in os.listdir(args.dataset) if name.endswith('.json')]
+    print("Total .json files: {}".format(len(json_file_names)))
 
-    train_counter = 0
-    val_counter = 0
-    inval_counter = 0
+    twin_file_names = []
 
     for j in json_file_names:
         ann = json.load(open(os.path.join(args.dataset, j)))
-        if not os.path.exists(os.path.join(args.dataset, ann['imagePath'])):
-            inval_counter += 1
+        imagePath = ann['imagePath']
+        if not os.path.exists(os.path.join(args.dataset, imagePath)):
             continue
-        rand = random.random()
-        if rand < args.ratio:
-            shutil.move(os.path.join(args.dataset, j), os.path.join(args.dataset, 'val'))
-            shutil.move(os.path.join(args.dataset, ann['imagePath']), os.path.join(args.dataset, 'val'))
-            val_counter += 1
-        else:
-            shutil.move(os.path.join(args.dataset, j), os.path.join(args.dataset, 'train'))
-            shutil.move(os.path.join(args.dataset, ann['imagePath']), os.path.join(args.dataset, 'train'))
-            train_counter += 1
+        twin_file_names.append((j, imagePath))
 
-    print("train file num: {}\n"
-          "val file num: {}\n"
-          "invalid file num: {}".format(train_counter, val_counter, inval_counter))
+    n = len(twin_file_names)
+    rand_val_ix = random.sample(range(n), int(args.ratio * n))
+    for ix, t in enumerate(twin_file_names):
+        dst = 'train'
+        if ix in rand_val_ix:
+            dst = 'val'
+        shutil.move(os.path.join(args.dataset, t[0]), os.path.join(args.dataset, dst))
+        shutil.move(os.path.join(args.dataset, t[1]), os.path.join(args.dataset, dst))
+
+    print("Invalid .json files: {}".format(len(json_file_names) - n))
+    print("Move to train: {}".format(n - int(args.ratio * n)))
+    print("Move to val: {}({}%)".format(int(args.ratio * n), args.ratio * 100))

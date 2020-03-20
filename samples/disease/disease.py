fe2678540d9b60c20132e901fef49096377075f0
Chienping Tsung <jianpingzong@outlook.com>
2020-03-20 17:08:52 +0800

    Update disease.py

--- samples/disease/disease.py
+++ samples/disease/disease.py
@@ -288,15 +288,47 @@ def train(model):
     dataset_val.load_disease(args.dataset, "val")
     dataset_val.prepare()
 
+    # Augmentation settings
+    from imgaug import augmenters as iaa
+    aug = iaa.Sequential([
+        iaa.Fliplr(0.5), # horizontal flips
+        iaa.Flipud(0.5), # vertical flips
+        iaa.Crop(percent=(-0.25, 0.25)), # random crops
+        # Small gaussian blur with random sigma between 0 and 0.5.
+        # But we only blur about 50% of all images.
+        iaa.Sometimes(
+            0.5,
+            iaa.GaussianBlur(sigma=(0, 0.5))
+        ),
+        # Strengthen or weaken the contrast in each image.
+        iaa.LinearContrast((0.75, 1.5)),
+        # Add gaussian noise.
+        # For 50% of all images, we sample the noise once per pixel.
+        # For the other 50% of all images, we sample the noise per pixel AND
+        # channel. This can change the color (not only brightness) of the
+        # pixels.
+        iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+        # Make some images brighter and some darker.
+        # In 20% of all cases, we sample the multiplier once per channel,
+        # which can end up changing the color of the images.
+        iaa.Multiply((0.8, 1.2), per_channel=0.2),
+        # Apply affine transformations to each image.
+        # Scale/zoom them, translate/move them, rotate them and shear them.
+        iaa.Affine(
+            scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+            rotate=(-25, 25),
+            shear=(-8, 8)
+        )
+        ], random_order=True)
+
     # *** This training schedule is an example. Update to your needs ***
-    # Since we're using a very small dataset, and starting from
-    # COCO trained weights, we don't need to train too long. Also,
-    # no need to train all layers, just the heads should do it.
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=30,
-                layers='heads')
+                layers='all',
+                augmentation=aug)
 
 
 def color_splash(image, mask):
